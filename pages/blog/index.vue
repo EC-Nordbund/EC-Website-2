a2d224e851ff5bc5afb07b3faaadc29cc93259a0
Sebastian Krüger <2pi_r2@gmx.de>
2020-07-24 09:15:36 +0200

    add pagination

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -10,17 +10,42 @@
             span(style="margin-right: 20px;") {{item.title}}
             v-chip(color="secondary" v-for="tag in item.tags" :key="tag") {{ tag }}
           v-list-item-subtitle {{item.description}}
+    v-pagination(
+      v-model="page"
+      :length="pagesCount"
+      :total-visible="7"
+    )
 </template>
 <script>
 export default {
-  async asyncData({ $content }) {
+  async asyncData({ $content, query }) {
+    const page = parseInt(query.page || '1')
 
-    // const sub2 = await $content('veranstaltung').fetch()
-    // console.log(sub2)
+    const skip = page * 10 - 10
 
-    const posts = await $content('blog').without(['body']).sortBy('published', 'desc').fetch()
+    const posts = await $content('blog').without(['body']).sortBy('published', 'desc').limit(10).skip(skip).fetch()
 
-    return { posts }
+    const pagesCount = Math.ceil((await $content('blog').without(['body', 'title', 'description','featuredImage']).fetch()).length / 10)
+
+    return { posts, page, pagesCount }
   },
+  data() {
+    return {
+      posts: [],
+      page: 0,
+      pagesCount: 1
+    }
+  },
+  methods: {
+    async goToPage() {
+      this.$router.replace({path: '/blog', query: { page: this.page }})
+      this.posts  = await this.$content('blog').without(['body']).sortBy('published', 'desc').limit(10).skip(this.page*10-10).fetch()
+    },
+  },
+  watch: {
+    page: {
+      handler: 'goToPage'
+    }
+  }
 }
 </script>
