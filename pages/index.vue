986edab68a50ba2b9da72b6473ade1bb238b4b50
Sebastian Krüger <2pi_r2@gmx.de>
2020-07-29 11:34:18 +0200

    some fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template lang="pug">
-  div Startseite &#xec01; &#xec02; &#xec03; &#xec04;
+  div Startseite  
     h2 Unsere 4 Grundsätze
+    table
+      tbody(style="font-weight: 500;")
+        tr
+          td(style="color: var(--v-up-base); text-align: right;") &#xec02; 
+          td entschieden für Cristus
+        tr
+          td(style="color: var(--v-in-base); text-align: right;") &#xec01; 
+          td zugehörig zur Gemeinde
+        tr 
+          td(style="color: var(--v-with-base); text-align: right;") &#xec03; 
+          td verbunden mit allen Christen
+        tr 
+          td(style="color: var(--v-out-base); text-align: right;") &#xec04;
+          td gesandt in die Welt
     v-img(:src="require('~/assets/img/Grundsätze.png')" width="90%")
 </template>
  
--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,6 @@ module.exports = {
   // https://stylelint.io/user-guide/configuration
   rules: {
     'font-family-no-missing-generic-family-keyword': null,
-    ignoreAtRules: ['function', 'if', 'for'],
+    'at-rule-no-unknown': null,
   },
 }
