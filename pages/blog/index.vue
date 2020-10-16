b2338714d00cbbdade4425b35a9dd6bcca16a5cd
Tobias <tobias_krause@akranet.de>
2020-10-16 02:09:47 +0200

    design for blog overview

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,14 +1,45 @@
 <template lang="pug">
   v-container
-    v-list(three-line)
-      v-list-item(v-for="item in posts" @click="$router.push(`/blog/${item.slug}`)" :key="item.slug")
-        v-list-item-avatar
-          v-img(:src="item.featuredImage")
-        v-list-item-content
-          v-list-item-title 
-            span(style="margin-right: 20px;") {{item.title}}
-            v-chip(color="secondary" v-for="tag in item.tags" :key="tag") {{ tag }}
-          v-list-item-subtitle {{item.description}}
+    h2 Beiträge
+    //- TODO: filter by tags, author, source (if instagram-feed is included)
+    v-row
+      v-col(v-for="item in posts" cols="12" :key="item.slug")
+        v-card(outlined tile hover class="overflow-hidden" color="offWhite")
+          v-row(no-gutters @click="$router.push(`/blog/${item.slug}`)")
+            v-col(cols="12" sm="6" md="5" lg="4" class="hellGrau")
+                //- image
+                v-img(:src="item.featuredImage" :height="imgHeight" aspectRatio="1" class="white--text" gradient="180deg, rgba(0,0,0,0.16) 0%, rgba(0,0,0,0.02) 24%, rgba(0,0,0,0.02) 64%, rgba(0,0,0,0.24) 100%")
+                  v-card-actions(class="pa-3 hidden-sm-and-up")
+                    v-spacer
+                    v-btn(color="accent" class="ec-gradient" elevation="16" fab tile @click="$router.push(`/blog/${item.slug}`)")
+                      //- TODO: hexagon shape for button (maybe in a custom component)
+                      v-icon(color="white" size="28") mdi-arrow-right
+            v-col(cols="12" sm="6" md="7" lg="8" class="d-flex flex-column justify-space-between" :style="detailsMaxHeight")
+              d-flex()
+                //- title
+                div(class="ec-gradient white--text")
+                  v-card-title(class="d-block pt-2 font-weight-bold text-truncate") {{item.title}}
+                  v-card-subtitle(class="pb-2 secondary--text d-flex justify-space-between")
+                    span Vom {{item.published.split('T')[0].split('-').reverse().join('.')}}
+                v-card-text(class="full-heigth overflow-hidden py-0 hidden-xs-only")
+                  //- labels
+                  v-row(no-gutters)
+                    //- categories
+                    v-col(cols="12" lg="8" class="d-flex mb-1 ml-n2 mt-3")
+                      v-chip(color="secondary" text-color="primary" class="ml-2 font-weight-medium" outlined small v-for="tag in item.tags" :key="tag")
+                        | {{ tag }}
+                  //- description
+                  v-row
+                    v-col(cols="12") {{getDescription(item)}}
+
+              //- actions/buttons
+              v-card-actions(class="pa-4 hidden-xs-only")
+                v-spacer
+                v-btn(color="primary" class="ec-gradient" depressed tile large @click="$router.push(`/blog/${item.slug}`)")
+                  //- TODO: hexagon shape for button (maybe in a custom component)
+                  | Weiter Lesen
+                  v-icon mdi-arrow-right
+
     v-pagination(
       v-model="page"
       :length="pageCount"
@@ -19,7 +50,7 @@
 const pagination = {
   getPostsOfPage($content, page) {
     return $content('blog')
-      .only(['title', 'tags', 'description', 'featuredImage', 'slug'])
+      .only(['title', 'tags', 'description', 'featuredImage', 'slug', 'published'])
       .sortBy('published', 'desc')
       .skip(10 * (page - 1))
       .limit(10)
@@ -32,6 +63,34 @@ const pagination = {
 }
 
 export default {
+  computed: {
+    detailsMaxHeight() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+        case "sm":
+          return "";
+        default:
+          return "max-height: 300px;";
+      }
+    },
+    imgHeight() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+        case "sm":
+          return undefined;
+        default:
+          return 300;
+      }
+    }
+  },
+  methods: {
+    getDescription(item) {
+      if(typeof item.description == "string" && item.description.length > 0) {
+        return item.description;
+      }
+      return 'Klicke auf "Weiter Lesen" um den Betrag zu vollständig zu lesen.';
+    }
+  },
   async asyncData({ $content, query }) {
     const page = parseInt(query.page || '1') || 1
 
