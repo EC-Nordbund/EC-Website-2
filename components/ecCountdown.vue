976506534d5ec8936f86bb0f10d01cde6faafdc4
Tobias <tobias_krause@akranet.de>
2020-10-17 22:38:23 +0200

    refactor countdown component

--- components/ecCountdown.vue
+++ components/ecCountdown.vue
@@ -1,22 +1,37 @@
-<template>
-  <ul class="ec-countdown">
-    <li v-if="days > 0">
-      <p class="digit">{{ twoDigits(days) }}</p>
-      <p class="text">{{ days > 1 ? 'Tage' : 'Tag' }}</p>
-    </li>
-    <li v-if="(days > 0 || hours > 0)">
-      <p class="digit">{{ twoDigits(hours) }}</p>
-      <p class="text">{{ hours > 1 ? 'Stunden' : 'Stunde' }}</p>
-    </li>
-    <li v-if="(days > 0 || hours > 0 || minutes > 0)">
-      <p class="digit">{{ twoDigits(minutes) }}</p>
-      <p class="text">{{ minutes > 1 ? 'Minuten' : 'Minute' }}</p>
-    </li>
-    <li>
-      <p class="digit">{{ twoDigits(seconds) }}</p>
-      <p class="text">{{ seconds > 1 ? 'Sekunden' : 'Sekunde' }}</p>
-    </li>
-  </ul>
+<template lang="pug">
+  div(class="ec-countdown")
+    //- days
+    div(v-if="days > 0" class="counter-days")
+      slot(name="digits" :digits="formatDigits(days)")
+        span(class="digits") {{formatDigits(days)}}
+
+      slot(name="units" :unit="dayLabel" v-if="!hideUnits")
+        span(class="unit") {{dayLabel}}
+
+    //- hours
+    div(v-if="(days > 0 || hours > 0)" class="counter-hours")
+      slot(name="digits" :digits="formatDigits(hours)")
+        span(class="digits") {{formatDigits(hours)}}
+
+      slot(name="units" :unit="hourLabel" v-if="!hideUnits")
+        span(class="unit") {{hourLabel}}
+
+    //- minute
+    div(v-if="(days > 0 || hours > 0 || minutes > 0)" class="counter-minutes")
+      slot(name="digits" :digits="formatDigits(minutes)")
+        span(class="digits") {{formatDigits(minutes)}}
+
+      slot(name="units" :unit="minuteLabel" v-if="!hideUnits")
+        span(class="unit") {{minuteLabel}}
+
+    //- seconds
+    div(class="counter-seconds")
+      slot(name="digits" :digits="formatDigits(seconds)")
+        span(class="digits") {{formatDigits(seconds)}}
+
+      slot(name="units" :unit="secondLabel" v-if="!hideUnits")
+        span(class="unit") {{secondLabel}}
+
 </template>
 <script>
 import {
@@ -31,6 +46,10 @@ import {
 export default defineComponent({
   props: {
     target: String,
+    hideUnits: {
+      type: Boolean,
+      default: false
+    }
   },
   setup(props) {
     const now = shallowRef(new Date())
@@ -39,9 +58,14 @@ export default defineComponent({
       Math.trunc((target.getTime() - now.value.getTime()) / 1000)
     )
     const days = computed(() => Math.trunc(diff.value / 60 / 60 / 24))
-    const hours = computed(() => Math.trunc(diff.value / 60 / 60) & 24)
+    const hours = computed(() => (Math.trunc(diff.value / 60 / 60) & 24))
     const minutes = computed(() => Math.trunc(diff.value / 60) % 60)
     const seconds = computed(() => Math.trunc(diff.value) % 60)
+
+    const dayLabel = computed(() => days.value !== 1 ? 'Tage' : 'Tag')
+    const hourLabel = computed(() => hours.value !== 1 ? 'Stunden' : 'Stunde')
+    const minuteLabel = computed(() => minutes.value !== 1 ? 'Minuten' : 'Minute')
+    const secondLabel = computed(() => seconds.value !== 1 ? 'Sekunden' : 'Sekunde')
 
     const ended = computed(() => diff.value < 0)
 
@@ -60,57 +84,40 @@ export default defineComponent({
       }
     })
 
-    const twoDigits = (value) => {
+    const formatDigits = (value) => {
       if (value.toString().length <= 1) {
         return '0' + value.toString()
       }
       return value.toString()
     }
 
-    return { days, twoDigits, hours, minutes, seconds }
+    return { days, formatDigits, hours, minutes, seconds, dayLabel, hourLabel, minuteLabel, secondLabel }
   },
 })
 </script>
-
-<style>
-.heading {
-  color: #8fb217;
-  font-size: 20px;
-}
+<style lang="scss" scoped>
 .ec-countdown {
-  text-align: center;
-  padding: 1em;
-  margin: 0;
-  min-width: 320px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  > div {
+    padding: 0 6px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 }
-p {
-  margin: 0;
-}
-.ec-countdown li {
-  display: inline-block;
-  margin: 0 8px;
-  text-align: center;
-  position: relative;
-}
-.ec-countdown li p {
-  margin: 0;
-}
-.ec-countdown li:first-of-type {
-  margin-left: 0;
-}
-.ec-countdown li:last-of-type {
-  margin-right: 0;
-}
-.ec-countdown .digit {
+
+.digits {
   font-size: 32px;
   font-weight: 600;
-  line-height: 1.4;
-  margin-bottom: 0;
-  color: #8fb217;
+  margin-bottom: 4px;
 }
-.ec-countdown .text {
+
+.unit {
   text-transform: uppercase;
-  margin-bottom: 0;
   font-size: 12px;
 }
 </style>
