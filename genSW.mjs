0f566553e1026b871b1934ea112167d2e6270d06
Sebastian Krüger <2pi_r2@gmx.de>
2020-10-19 20:01:16 +0200

    add sw build

--- genSW.mjs
+++ genSW.mjs
@@ -1,12 +1,13 @@
-import { readdirSync, statSync } from 'fs'
+import { readdirSync, readFileSync, statSync, writeFileSync, existsSync } from 'fs'
+import { createHash } from 'crypto'
 import { join } from 'path'
 
 const __ROOT__ = '.nuxt/dist/client/'
 
-const ASSETS = ['.nuxt/dist/client/empty']
+const ASSETS = []
 
 function readDir(dir) {
-  readdirSync(dir).forEach(f => {
+  readdirSync(dir).forEach((f) => {
     const ff = join(dir, f)
 
     if (statSync(ff).isFile()) {
@@ -21,6 +22,27 @@ function readDir(dir) {
   })
 }
 
-readDir(__ROOT__)
+if (existsSync(__ROOT__)) {
+  readDir(__ROOT__)
+  const sha = createHash('md5')
+  const baseSW = readFileSync('./swBase.js', 'utf-8')
 
-console.log(JSON.stringify(ASSETS.map(v => '_nuxt/' + v.split(/\\|\//).join('/').slice(__ROOT__.length))))
+  sha.update(JSON.stringify(ASSETS) + baseSW)
+
+  const SW_DATA = {
+    VERSION: sha.digest('base64'),
+    ASSETS: ASSETS.map(
+      (v) => '_nuxt/' + v.split(/\\|\//).join('/').slice(__ROOT__.length)
+    ),
+    PAGES: ['empty'],
+  }
+
+  const SW = `
+const SW_DATA = ${JSON.stringify(SW_DATA)}
+${baseSW}
+`
+
+  writeFileSync('./static/sw.js', SW)
+} else {
+  writeFileSync('./static/sw.js', '// NO-OP Dev SW')
+}
