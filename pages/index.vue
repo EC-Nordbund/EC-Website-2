c18a00dc6452369ed49eca56ae9adf7d32a703d7
Sebastian Krüger <2pi_r2@gmx.de>
2020-10-20 16:44:27 +0200

    better design

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,73 @@
 <template lang="pug">
-  div
-    v-container
-      div(class="d-flex flex-row justify-space-between align-end")
-        h2(id="aktuelles") Aktuelles
-        v-btn(text depressed tile large @click="$router.push(`/blog/`)")
-          span(class="hidden-xs-only") Mehr Beiträge
-          v-icon(class="ml-1 mr-n1") mdi-arrow-right
-      v-row
-        v-col(cols="12" sm="6" md="4" v-for="item in recentPosts" :key="item.slug")
-          v-card(tile hover outlined @click="$router.push(`/blog/${item.slug}`)")
-            ec-image-item(:image="item.featuredImage" :title="item.title" :subTitle="`Vom ${item.published.split('T')[0].split('-').reverse().join('.')}`")
-    v-container
-      div(class="d-flex flex-row justify-space-between align-end")
-        h2(id="nächste-veranstaltungen") Nächste Veranstaltungen
-        v-btn(text depressed tile large @click="$router.push(`/veranstaltungen/`)")
-          span(class="hidden-xs-only") Mehr Veranstaltungen
-          v-icon(class="ml-1 mr-n1") mdi-arrow-right
-      v-row
-        v-col(cols="12" sm="6" md="4" v-for="item in upcomingEvents" :key="item.slug")
-          v-card(tile hover outlined @click="$router.push(`/veranstaltungen/${item.slug}`)")
-            ec-image-item(:image="item.featuredImage" :title="item.title" :subTitle="`Vom ${item.begin.split('-').reverse().join('.')} bis ${item.ende.split('-').reverse().join('.')}`")
-    v-container
-        h2(id="über-uns") Über uns
-        p 
-          | Der EC-Nordbund ist einer von 18 Landesverbänden des Deutschen EC-Verbandes.
-          | EC bedeutet: „Entschieden für Christus“ und markiert die grundsätzlich evangelische Ausrichtung aller Aktivitäten.
-          | Im EC-Nordbund sind alle EC-Kinder- und Jugendarbeiten aus Schleswig-Holstein und Hamburg.
+div
+  div(
+    style='padding: 32px 79px 100px 79px; background: var(--v-offWihte-base); clip-path: polygon(0 0, 0 100%, 100% calc(100% - 3.492vw), 100% 0);margin-bottom: -68px;'
+  )
+    //- v-container
+    .d-flex.flex-row.justify-space-between.align-end
+      h2#aktuelles Aktuelles
+      v-btn(text, depressed, tile, large, @click='$router.push(`/blog/`)')
+        span.hidden-xs-only Mehr Beiträge
+        v-icon.ml-1.mr-n1 mdi-arrow-right
+    v-row
+      v-col(
+        cols='12',
+        sm='6',
+        md='4',
+        v-for='item in recentPosts',
+        :key='item.slug'
+      )
+        v-card(
+          tile,
+          hover,
+          outlined,
+          @click='$router.push(`/blog/${item.slug}`)'
+        )
+          ec-image-item(
+            :image='item.featuredImage',
+            :title='item.title',
+            :subTitle='`Vom ${item.published.split("T")[0].split("-").reverse().join(".")}`'
+          )
+    //- v-container
+    .d-flex.flex-row.justify-space-between.align-end
+      h2(id='nächste-veranstaltungen') Nächste Veranstaltungen
+      v-btn(
+        text,
+        depressed,
+        tile,
+        large,
+        @click='$router.push(`/veranstaltungen/`)'
+      )
+        span.hidden-xs-only Mehr Veranstaltungen
+        v-icon.ml-1.mr-n1 mdi-arrow-right
+    v-row
+      v-col(
+        cols='12',
+        sm='6',
+        md='4',
+        v-for='item in upcomingEvents',
+        :key='item.slug'
+      )
+        v-card(
+          tile,
+          hover,
+          outlined,
+          @click='$router.push(`/veranstaltungen/${item.slug}`)'
+        )
+          ec-image-item(
+            :image='item.featuredImage',
+            :title='item.title',
+            :subTitle='`Vom ${item.begin.split("-").reverse().join(".")} bis ${item.ende.split("-").reverse().join(".")}`'
+          )
+    //- v-container
+  div(
+    style='padding: 100px 79px; background: var(--v-primary-base); clip-path: polygon(0 3.492vw, 0 calc(100% - 3.492vw), 100% 100%, 100% 0); margin-bottom: -68px;'
+  )
+    h2(id='über-uns') Über uns
+    p 
+      | Der EC-Nordbund ist einer von 18 Landesverbänden des Deutschen EC-Verbandes.
+      | EC bedeutet: „Entschieden für Christus“ und markiert die grundsätzlich evangelische Ausrichtung aller Aktivitäten.
+      | Im EC-Nordbund sind alle EC-Kinder- und Jugendarbeiten aus Schleswig-Holstein und Hamburg.
 </template>
 <script>
 export default {
