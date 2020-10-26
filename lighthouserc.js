cca09bc503700ef02ca020617dd3de02d666e670
Sebastian Krüger <2pi_r2@gmx.de>
2020-10-26 12:35:20 +0100

    edit LH config

--- lighthouserc.js
+++ lighthouserc.js
@@ -10,13 +10,23 @@ module.exports = {
       // token: 'Your *build token* goes here', // could also use LHCI_TOKEN variable instead
     },
     // TODO: Überlegen was hier sinn macht.
-    // assert: {
-    // preset: 'lighthouse:recommended',
-    // assertions: {
-    // "first-contentful-paint": "off",
-    // "works-offline": ["warn", { "minScore": 1 }],
-    // "uses-responsive-images": ["error", { "maxLength": 0 }]
-    // }
-    // },
+    assert: {
+      preset: 'lighthouse:recommended',
+      // assertions: {
+      // "first-contentful-paint": "off",
+      // "works-offline": ["warn", { "minScore": 1 }],
+      // "uses-responsive-images": ["error", { "maxLength": 0 }]
+      // },
+      "assertions": {
+        "categories:performance": ["error", { "minScore": 0.85 }],
+        "categories:accessibility": ["error", { "minScore": 1 }],
+        "categories:pwa": ["error", { "minScore": 0.9 }],
+        "categories:seo": ["error", { "minScore": 0.9 }],
+        "categories:best-practices": ["error", { "minScore": 0.9 }],
+        "color-contrast": "off",
+        "redirects-http": "off",
+        "tap-targets": "off"
+      }
+    },
   }
 }
