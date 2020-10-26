05e9281c06dc80df5e87ee4dc4d0e94ee534b96b
Sebastian Krüger <2pi_r2@gmx.de>
2020-10-26 17:43:19 +0100

    fix test config

--- lighthouserc.js
+++ lighthouserc.js
@@ -15,8 +15,8 @@ module.exports = {
       "assertions": {
         "categories:performance": ["error", { "minScore": 0.80 }],
         "categories:accessibility": ["error", { "minScore": 0.98 }],
-        "categories:pwa": ["error", { "minScore": 0.95 }],
-        "categories:seo": ["error", { "minScore": 0.9 }],
+        "categories:pwa": ["error", { "minScore": 0.93 }],
+        "categories:seo": ["error", { "minScore": 1 }],
         "categories:best-practices": ["error", { "minScore": 1 }],
         // User Farbschema geht sonst nicht
         "color-contrast": "off",
