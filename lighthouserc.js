12aa04c51ee2fe0b80f2e70cb6657f684e40b85d
Sebastian Krüger <2pi_r2@gmx.de>
2020-10-26 17:30:40 +0100

    change fail params

--- lighthouserc.js
+++ lighthouserc.js
@@ -13,9 +13,9 @@ module.exports = {
     assert: {
       preset: 'lighthouse:recommended',
       "assertions": {
-        "categories:performance": ["error", { "minScore": 0.85 }],
+        "categories:performance": ["error", { "minScore": 0.80 }],
         "categories:accessibility": ["error", { "minScore": 0.98 }],
-        "categories:pwa": ["error", { "minScore": 0.85 }],
+        "categories:pwa": ["error", { "minScore": 0.95 }],
         "categories:seo": ["error", { "minScore": 0.9 }],
         "categories:best-practices": ["error", { "minScore": 1 }],
         // User Farbschema geht sonst nicht
