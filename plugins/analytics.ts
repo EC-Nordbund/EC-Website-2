2586f4ed8058d8338c08229024ea577af17c0b36
Sebastian Krüger <2pi_r2@gmx.de>
2020-10-27 22:28:00 +0100

    new analytics link

--- plugins/analytics.ts
+++ plugins/analytics.ts
@@ -6,8 +6,8 @@ export default defineNuxtPlugin((context) => {
   if (navigator.doNotTrack != '1') {
     const ackee = create(
       {
-        server: 'https://next.analytics.ec-nordbund.de',
-        domainId: '12ac1013-cf6c-4515-89b4-64ea9514b3e0',
+        server: 'https://analytics.ec-nordbund.de',
+        domainId: 'bab181a1-5923-45dd-a03f-4b21338a9d23',
       },
       {
         ignoreLocalhost: true,
