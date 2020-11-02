efed2091b9fef6b469ac1cbdd548c0e8b58c3dd6
Sebastian Krüger <2pi_r2@gmx.de>
2020-11-03 00:08:55 +0100

    add base file

---
+++ pages/anmeldung/token.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  v-container
+    //- | WENN DU DAS LIEST IST WAS SCHIEFGEGANGEN...
+</template>
+<script>
+export default {
+ setup(_, ctx) {
+   // Überprüfen des Tokens baut @mathe42 selber ein. Wenn fehlerhaft wird auf 404 weitergeleitet.
+    if(ctx.root.$route.query.token) {
+      if(true) { // Erstmal immer wahr
+        // Verrify Token normal (MA anmeldung oder TN anmeldung oder MA anmeldung Ort)
+      } else {
+        // MA anmeldung Ort verify durch verantwortlichen
+      }
+    } else {
+      // Zeige Token eingabefeld.
+    }
+ }
+}
+</script>
