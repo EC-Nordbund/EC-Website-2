463cef19b09ee8946f52167b2d21bdf24efd31a8
Sebastian Krüger <2pi_r2@gmx.de>
2020-11-07 10:52:39 +0100

    add SMTP

--- api/sendMail.ts
+++ api/sendMail.ts
@@ -5,16 +5,28 @@ let transport: ReturnType<typeof createTransport> | null = null
 
 export const sendMail: ReturnType<typeof createTransport>['sendMail'] = async (...args: any[]) => {
   if (!transport) {
-    const testAccount = await createTestAccount()
-    transport = createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
-      },
-    })
+    if(process.env.ON_SERVER) {
+      transport = createTransport({
+        host: process.env.SMTP_HOST,
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+      })
+    } else {
+      const testAccount = await createTestAccount()
+      transport = createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: testAccount.user, // generated ethereal user
+          pass: testAccount.pass, // generated ethereal password
+        },
+      })
+    }
   }
 
   const info = await (transport.sendMail as any)(...args)
